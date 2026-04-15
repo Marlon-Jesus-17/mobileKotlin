1ba53cce0d2b14403a3fcd3164110c232eb70d2b
Marlon <maarlonsantos17@gmail.com>
2026-04-15 20:37:30 -0300

    Meu primeiro programa android: Ler um nome e devolver uma mensagem de resposta

---
+++ MeuPrimeiroApp/app/src/main/java/com/marlebas/meuprimeiroapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.marlebas.meuprimeiroapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.marlebas.meuprimeiroapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            val nome = binding.editNome.text.toString()
+//            binding.textResultado.text = "Olá " + nome
+//            binding.textResultado.text = "Olá ${nome}"
+            binding.textResultado.setText("Olá ${nome}")
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
