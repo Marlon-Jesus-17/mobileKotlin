12707f1b9c686282f00afa7430eabae060a90edc
Marlon <maarlonsantos17@gmail.com>
2026-04-16 14:55:49 -0300

    Conversor de Dólar para real

---
+++ ConversorDolarReal/app/src/main/java/com/marlebas/conversordolarreal/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.marlebas.conversordolarreal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.marlebas.conversordolarreal.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.buttonConverter.setOnClickListener {
+            val dolar = binding.editDolar.text.toString().toDouble()
+            val real = String.format("%.2f", dolar * 5)
+            binding.textReal.setText("Valor em real: R$${real}")
+        }
+    }
+}
