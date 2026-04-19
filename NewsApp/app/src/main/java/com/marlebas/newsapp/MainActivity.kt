4380f8cf41aabfc77dc2933382f6106ee3e39550
Marlon <maarlonsantos17@gmail.com>
2026-04-19 18:51:33 -0300

    Adicionei LazyColumn para ter uma listagem mais leve e profissional; Dei permissão para o app usar a internet com a tag XML <uses-permission>

--- NewsApp/app/src/main/java/com/marlebas/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/marlebas/newsapp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -32,14 +36,18 @@ fun MinhaPrimeiraTela(viewModel: CounterViewModel = viewModel()){
 
     val posts = viewModel.posts.value
 
-    Column (modifier = Modifier.fillMaxSize()){
+    Column (modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
         Button(onClick = {
             viewModel.carregarPosts()
         }) {
             Text("Buscar Posts")
         }
-        posts.forEach {
-            Text(it.title)
+        LazyColumn {
+            items (posts){
+                post -> Text(post.title)
+            }
         }
     }
 }
