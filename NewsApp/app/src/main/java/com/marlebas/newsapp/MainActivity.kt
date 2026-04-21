327ab992672ab8c4d305c48e224a495e1c01e602
Marlon <maarlonsantos17@gmail.com>
2026-04-21 19:19:04 -0300

    Lista rolável, cards para separa as notícias, título + descrição

--- NewsApp/app/src/main/java/com/marlebas/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/marlebas/newsapp/MainActivity.kt
@@ -6,16 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.marlebas.newsapp.data.Post
 import com.marlebas.newsapp.ui.theme.NewsAppTheme
 import com.marlebas.newsapp.ui.viewModel.CounterViewModel
 
@@ -35,6 +43,7 @@ class MainActivity : ComponentActivity() {
 fun MinhaPrimeiraTela(viewModel: CounterViewModel = viewModel()){
 
     val posts = viewModel.posts.value
+    val isLoading = viewModel.isLoading.value
 
     Column (modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -44,13 +53,33 @@ fun MinhaPrimeiraTela(viewModel: CounterViewModel = viewModel()){
         }) {
             Text("Buscar Posts")
         }
+        if(isLoading){
+            Text("Carregando...")
+        }
         LazyColumn {
             items (posts){
-                post -> Text(post.title)
+                post -> PostItem(post)
             }
         }
     }
 }
+
+@Composable
+fun PostItem(post: Post){
+
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = post.title,
+                style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = post.body,
+                style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
--- NewsApp/app/src/main/java/com/marlebas/newsapp/ui/viewModel/CounterViewModel.kt
+++ NewsApp/app/src/main/java/com/marlebas/newsapp/ui/viewModel/CounterViewModel.kt
@@ -11,13 +11,19 @@ class CounterViewModel : ViewModel(){
     var posts = mutableStateOf<List<Post>>(emptyList())
         private set
 
+    var isLoading = mutableStateOf(false)
+        private set
+
     fun carregarPosts(){
         viewModelScope.launch {
             try {
+                isLoading.value = true
                 val response = RetrofitIntance.api.getPosts()
                 posts.value = response
             } catch (e: Exception){
                 e.printStackTrace()
+            } finally {
+                isLoading.value = false
             }
         }
     }
